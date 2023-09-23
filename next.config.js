b037fc7c0e118c76be551ef950690dcda945ab77
JaffaFriedman <jafriedman.dwfsc4@bootcampudd.cl>
2023-09-23 09:40:22 -0300

    Modificaciones configuración next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ["https://res.cloudinary.com/"],
         remotePatterns: [
           {
             protocol: 'https',
