e2f20f5e2434350f483005c35df6b74fe1380278
JaffaFriedman <jafriedman.dwfsc4@bootcampudd.cl>
2023-09-23 12:22:17 -0300

    Se agrega imagen de fondo

--- src/components/AnimatedImage.jsx
+++ src/components/AnimatedImage.jsx
@@ -1,29 +1,32 @@
 'use client'
 import React from 'react'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
-const desdeIzquierda={
-    initial:{
-      opacity:0,
-      x:-50,
-    },
-    animate:{
-      opacity:1,
-      x:0,
-      transition:{
-        duration:3,
-      }
+const desdeIzquierda = {
+  initial: {
+    opacity: 0,
+    x: -50
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 3
     }
-  
   }
+}
 
-export const AnimatedImage = ({src}) => {
+export const AnimatedImage = ({ src }) => {
   return (
     <>
-        <motion.div initial='initial' animate='animate' variants={desdeIzquierda} className='w-1/2 '>
-              <Image src={src} width={500} height={500}/>
-          </motion.div> 
-    
+      <motion.div
+        initial='initial'
+        animate='animate'
+        variants={desdeIzquierda}
+        className='w-1/2 '
+      >
+        <Image src={src} width={500} height={500} />
+      </motion.div>
     </>
   )
 }
